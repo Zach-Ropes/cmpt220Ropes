8da9ec0d2af76cf144e7e9d681d5cc1d5f7cbab8
Zachary Ropes <Zachary Ropes>
2017-02-23 16:15:55 -0500

    Zachary Ropes, February 23, 2017, Lab 3, Problem 5.1

---
+++ labs/3/Lab3_prob1.java
@@ -0,0 +1,40 @@
+// header
+//Problem 5.1
+
+import java.util.Scanner;
+
+public class Lab3_prob1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter an integer, the input ends if it is 0: ");
+    int num = input.nextInt();
+
+    double total = 0;
+    int pos = 0;
+    int neg = 0;
+    int count = 0;
+    
+    if (num == 0) {
+      System.out.println("No numbers are entered except 0");
+      }
+
+    do {
+      if (num > 0)
+        pos++;
+      else
+        neg++;
+    
+      total += num;
+      count++;
+      num = input.nextInt();
+      } while (num != 0);
+
+    double average = total / count;
+
+    System.out.println("The number of positive is " + pos);
+    System.out.println("The number of negatives is " + neg);
+    System.out.println("The total is total " + total);
+    System.out.println("The average is " + average);
+  }
+}
