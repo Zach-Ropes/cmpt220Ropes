817be87c5a45f0b5ac213105a3abd713a3a74cb7
Zachary Ropes <Zachary Ropes>
2017-02-23 16:17:37 -0500

    Zachary Ropes, February 23, 2017, Lab 3, Problem 6.2

---
+++ labs/3/Lab3_prob5.java
@@ -0,0 +1,24 @@
+// header
+// Problem 6.2
+
+import java.util.Scanner;
+
+public class Lab3_prob5 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("Enter an integer: ");
+    long num = input.nextLong();
+    
+    System.out.println("The sum of the digits of " + num + " is " + sumDigits(num));
+  }
+  //second method sumDigits
+  public static int sumDigits(long n) {
+    int sum = 0;
+    while (n > 0) {
+      sum += n % 10;
+      n = n / 10;      
+    }
+    return sum;
+  }
+}
