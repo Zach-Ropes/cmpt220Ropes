f6eafe7092a6738848522a08d09360fabb1e6d38
Zachary Ropes <Zachary Ropes>
2017-02-24 10:52:36 -0500

    Problem 5.12: Finds the smallest possible integer for n where n^2 is greater than 12,000

--- labs/3/Lab3_prob3.java
+++ labs/3/Lab3_prob3.java
@@ -1,5 +1,14 @@
-// header
-// Problem 5.12
+/**
+   * file: Lab3_prob3.java
+   * author: Zachary Ropes
+   * course: CMPT 220
+   * assignment: lab 3, problem 5.12
+   * February 23, 2017
+   * version: 1.1
+   * 
+   * Finds the smallest possible integer for n where
+   * n^2 is greater than 12,000.
+   */  
 
 public class Lab3_prob3 {
   public static void main(String[] args) {
