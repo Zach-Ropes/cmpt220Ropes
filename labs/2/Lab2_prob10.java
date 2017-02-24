03d68eafdc5e03a75d83b31ed73e58614bf5979e
Zachary Ropes <Zachary Ropes>
2017-02-24 10:42:49 -0500

    Problem 4.25: Generates a random car plate

--- labs/2/Lab2_prob10.java
+++ labs/2/Lab2_prob10.java
@@ -1,3 +1,14 @@
+/**
+   * file: Lab2_prob10.java
+   * author: Zachary Ropes
+   * course: CMPT 220
+   * assignment: lab 2, problem 4.25
+   * due date: February 9, 2017
+   * version: 1.1
+   * 
+   * Generates a random car plate  
+   */
+
 import java.util.Scanner;
 
 public class Lab2_prob10 {
