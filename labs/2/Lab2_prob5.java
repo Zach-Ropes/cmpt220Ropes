1e71819ee7327536ac0859f70465182c9f190956
Zachary Ropes <Zachary Ropes>
2017-02-24 10:37:15 -0500

    Problem 4.1: Find the area of a pentagon

--- labs/2/Lab2_prob5.java
+++ labs/2/Lab2_prob5.java
@@ -1,3 +1,14 @@
+/**
+   * file: Lab2_prob5.java
+   * author: Zachary Ropes
+   * course: CMPT 220
+   * assignment: lab 2, problem 4.1
+   * due date: February 9, 2017
+   * version: 1.1
+   * 
+   * Finds the area of a penatagon
+   */
+
 import java.util.Scanner;
 
 public class Lab2_prob5 {
@@ -10,6 +21,6 @@ public class Lab2_prob5 {
     double area = (5 * Math.pow(s, 2)) / (4 * Math.tan(Math.PI / 5));
     double round = Math.round(area * 100) / 100.00;
 
-    System.out.println("The are of the pentagon is " + round);
+    System.out.println("The area of the pentagon is " + round);
   }
 }
